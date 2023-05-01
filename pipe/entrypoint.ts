9728ef7fbfacf4af015204d1e83f94e25ba13ac1
Daniel Van Der Ploeg <daniel.vanderploeg@aligent.com.au>
2023-05-01 16:50:18 +0930

    feat: convert if statements to switch

--- pipe/entrypoint.ts
+++ pipe/entrypoint.ts
@@ -2,24 +2,25 @@ import { runCLICommand } from "./cmd"
 import { generateAuthToken } from "./jwt"
 import { env } from "./env"
 
-if (!env.command|| env.command === '')
-    throw new Error("⛔ No aio command specified")
-
 let commandStr: string[] = []
 
-
-if (env.command.toLowerCase() === 'build') {
-    commandStr.push("aio app build")
-}
-else if (env.command.toLowerCase() === 'deploy') {
+switch (env.command ? env.command.toLowerCase() : '') {
+  case '':
+    throw new Error("⛔ No aio command specified");
+  case 'build':
+    commandStr.push("aio app build");
+    break;
+  case 'deploy':
     generateAuthToken();
-    commandStr.push('aio app deploy --no-build')
-}
-else if (env.command.toLowerCase() === 'auth') {
-    generateAuthToken()
+    commandStr.push('aio app deploy --no-build');
+    break;
+  case 'auth':
+    generateAuthToken();
+    break;
 }
 
 if (env.debug) console.log(`ℹ️ Running pipeline in ${env.command} mode`);
+
 runCLICommand(commandStr)
     .then(() => {
         console.log("🕺 Pipeline run was successful")
